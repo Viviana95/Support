cce726855504d31a67151990fdf0579f3477b78f
Iria <iriargon94@gmail.com>
2022-07-26 13:49:33 +0200

    ruta wena

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-<?php
-
-require('vendor/autoload.php');
-require('src/app.php');
-=======
 
     <?php
 
     // require("vendor/autoload.php");
     require("./src/app.php");
 
-
     ?>
->>>>>>> 0bd938d1a8965d6c9816f8e3aedd56c57ec14588
