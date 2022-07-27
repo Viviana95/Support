dc92e0e9cfe2abcaf9eb615cf2f3d5f321ea2104
Iria <iriargon94@gmail.com>
2022-07-27 14:41:31 +0200

    navbar

--- src/Views/Components/Nav.php
+++ src/Views/Components/Nav.php
@@ -5,21 +5,21 @@
 
 
 <Nav>
-<nav class="navbar navbar-expand-lg bg-dark fixed-top d-flex">
+<nav class="navbar navbar-expand-lg bg-light fixed-top d-flex">
+<a class="navbar-logo" rel="home" href='#' title="Logo"> 
+  <img style="max-width:100px; margin-top: -7px; margin-left: 10px" src='/src/asset/logo.png'> 
+</a>
   <div class="container-fluid">
     <div class="collapse navbar-collapse" id="navbarNav">
-      <a class="navbar-brand" rel="home" href='#' title="Logo"> 
-        <img style="max-width:100px; margin-top: -7px;" src='/src/assets/logo.png'> 
-      </a>
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" style="color: white" aria-current="page" href="?action=index">LISTADO DE SOLICITUDES</a>
+          <a class="nav-link active" style="color: black" aria-current="page" href="?action=index">LISTADO DE SOLICITUDES</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" style="color: white" href="?action=create">AÑADIR SOLICITUD</a>
+          <a class="nav-link" style="color: black" href="?action=create">AÑADIR SOLICITUD</a>
         </li>
         <li class="nav-item">
-        <a class="nav-link" style="color: white" href="?action=EditClient">DETALLE</a>
+        <a class="nav-link" style="color: black" href="?action=EditClient">DETALLE</a>
         </li>
       
       </ul>
