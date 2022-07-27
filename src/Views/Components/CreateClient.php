360215133a48f0e91be6ec09405194349f156efb
Laura <lauragarciacastelao@hotmail.com>
2022-07-27 14:00:09 +0200

    Add changes to develop

--- src/Views/Components/CreateClient.php
+++ src/Views/Components/CreateClient.php
@@ -7,10 +7,10 @@
         <form method="post" action="?action=store" class="bg-trasparent text-dark border border-wrepeatarning border border-3 text-dark p-5 m-5 d-flex justify-content-evenly w-100 opacity-50" style="background-image: url('https://mdbootstrap.com/img/Photos/Others/images/33.jpg')" ;>
 
             <div class="name_theme d-flex flex-column">
-                <div class="card border border-dark border border-3 " style="width: 30rem; ">
+                <div class="card border border-success border border-3 " style="width: 30rem; ">
                     <div class="card-header">
-                        <div class="bg-info text-center light mb-4 ">
-                            <label for="exampleFormControlTextarea1" class="form-label" style> <strong> ADD </strong>📒 </label>
+                        <div class="bg-success text-center light mb-4 ">
+                            <label for="exampleFormControlTextarea1" class="form-label" style> <p class= "text-white"<strong> ADD </strong>📒 </p></label>
 
                         </div>
 
