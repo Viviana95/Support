546f222fc49cd43f4cb2d5ee6888d67231befd0d
Laura <lauragarciacastelao@hotmail.com>
2022-07-27 15:28:02 +0200

    Finish details to create client component

--- src/Views/Components/CreateClient.php
+++ src/Views/Components/CreateClient.php
@@ -1,41 +1,40 @@
-<body class="d-flex flex-column mb-3" style="background-color: #43372b ";
-">
-    <div class="container d-flex justify-content-center mt-5">
+<body class="d-flex flex-column mb-3" style="background-color: #43372b " ; ">
+    <div class=" container d-flex justify-content-center mt-5">
 
 
 
-        <form method="post" action="?action=store" class="img-fluid text-dark border border-dark border border-3 text-white p-2 m-5 d-flex justify-content-evenly w-100 " style="background-image: url('https://raw.githubusercontent.com/lauracastelao/image/main/assets/fondo.png')" ;>
+    <form method="post" action="?action=store" class="img-fluid text-dark border border-dark border border-3 text-white p-2 m-5 d-flex justify-content-evenly w-100 " style="background-image: url('https://raw.githubusercontent.com/lauracastelao/image/main/assets/fondo.png')" ;>
 
-            <div class="name_theme d-flex flex-column">
-                <div class="card border border-dark border border-3 " style="width: 30rem; ">
-                    <div class="card-header">
-                        <div class="bg-secondary text-center light mb-4 ">
-                            <label for="exampleFormControlTextarea1" class="form-label" style> <strong> ADD </strong>📒 </p></label>
+        <div class="name_theme d-flex flex-column">
+            <div class="card border border-dark border border-3 " style="width: 30rem; ">
+                <div class="card-header">
+                    <div class="bg-secondary text-center light mb-4 ">
+                        <label for="exampleFormControlTextarea1" class="form-label" style> <strong> ADD </strong>📒 </p></label>
 
-                        </div>
+                    </div>
 
-                        <div class="mb-2">
-                            <label for="exampleFormControlInput1" class="form-label text-dark"><strong>Nombre</strong></label>
-                            <input name="client" class="form-control" id="Input1" placeholder="Nombre">
-                        </div>
-                        <div class="mb-2">
-                            <label for="exampleFormControlInput1" class="form-label text-dark"><strong>Tema</strong></label>
-                            <input name="issue" class="form-control" id="Input2" placeholder="Tema">
-                        </div>
+                    <div class="mb-2">
+                        <label for="exampleFormControlInput1" class="form-label text-dark"><strong>Nombre</strong></label>
+                        <input name="client" class="form-control" id="Input1" placeholder="Nombre">
+                    </div>
+                    <div class="mb-2">
+                        <label for="exampleFormControlInput1" class="form-label text-dark"><strong>Tema</strong></label>
+                        <input name="issue" class="form-control" id="Input2" placeholder="Tema">
+                    </div>
 
-                        <div class="bg-light text light mb-4">
-                            <label for="exampleFormControlInput1" class="form-label text-dark"><strong>Descripción</strong></label>
-                            <textarea name="description" class="form-control" id="Textarea1" rows="8"></textarea>
-                        </div>
+                    <div class="bg-light text light mb-4">
+                        <label for="exampleFormControlInput1" class="form-label text-dark"><strong>Descripción</strong></label>
+                        <textarea name="description" class="form-control" id="Textarea1" rows="8"></textarea>
                     </div>
                 </div>
-                <div class="botones d-flex justify-content-evenly mt-5" style="margin-bottom:2rem">
-                    <input class="btn-mg btn-outline-trasparent go-add-task float" type="submit" value="❌">
-                    <input class="btn-mg btn-outline-trasparent go-add-task float" type="reset" value="🔁">
-                    <input class="btn-mg btn-outline-trasparentgo-add-task float" type="submit" value="✔️">
-                </div>
+            </div>
+            <div class="botones d-flex justify-content-evenly mt-5" style="margin-bottom:2rem">
+                <input class="btn-mg btn-outline-trasparent go-add-task float" type="submit" value="❌">
+                <input class="btn-mg btn-outline-trasparent go-add-task float" type="reset" value="🔁">
+                <input class="btn-mg btn-outline-trasparentgo-add-task float" type="submit" value="✔️">
+            </div>
 
-        </form>
+    </form>
     </div>
     </div>
 
